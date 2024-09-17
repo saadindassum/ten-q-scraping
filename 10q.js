a9a3dfe3e9f21e4deb8b34872eac5b59f1a866e4
Saadin Dassum <34032935+saadindassum@users.noreply.github.com>
2024-09-17 10:50:26 -0500

    Removed try catch from parse10Q

--- 10q.js
+++ 10q.js
@@ -28,19 +28,15 @@ export default class TenQUtility {
      */
     async parse10Q(page, link) {
         // console.log(`Parsing link ${link}`);
-        try {
-            await page.goto(link, { waitUntil: 'networkidle0' });
+        await page.goto(link, { waitUntil: 'networkidle0' });
 
-            // First off, we want to know if it's the ultimate edge case - barebones.
-            let preHandle = await page.$('body > pre');
-            if (preHandle != null) {
-                return await this.parseBarebones(page, preHandle);
-            }
-
-            return await this.parseOrganized(page);
-        } catch (e) {
-            return [];
+        // First off, we want to know if it's the ultimate edge case - barebones.
+        let preHandle = await page.$('body > pre');
+        if (preHandle != null) {
+            return await this.parseBarebones(page, preHandle);
         }
+
+        return await this.parseOrganized(page);
     }
 
     /**
