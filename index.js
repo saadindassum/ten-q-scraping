b1f95a7aa0097c52494e47dfbcad4b4df813edce
saadindassum <34032935+saadindassum@users.noreply.github.com>
2024-10-06 12:16:59 -0500

    Created more efficient main
    This main uses parallel processing to parse filings rather than different CIKs.

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ async function main() {
   // We have those stored at searches.txt
   const searches = await getLines('searches.txt');
 
+  // NEW MAIN:
+  // Find CIK's
+  // Initialize cluster with parsing functionality
+  // For each CIK:
+  // - Make a browser, use it to find links.
+  // - Close the broswer
+  // - Use cluster to simultaneously parse filings. Catch errors instead of stopping the program.
+  //   * If what you want is to debug, use pushthrough() instead of main() at the bottom.
+  // - Throw each filing into the CIK's production folder.
+  // - Await until Cluster idle, then continue onto the next CIK
+
   // This time around we're using clusters
   // That will speed things up.
   const cluster = await Cluster.launch({
@@ -31,24 +42,41 @@ async function main() {
 
   await initCluster(cluster);
 
-  let skipList = getSkipList();
-  console.log(`Skipping:\n${skipList}`);
-
+  // // We're not using the big skip list for now.
+  // let skipList = getSkipList();
+  // console.log(`Skipping:\n${skipList}`);
+  let ciks = new Array();
   for (var i = 0; i < searches.length; i++) {
     let cik = searches[i];
     //If we don't do this, we won't get any hits on EDGAR
     while (cik.length < 10) {
       cik = '0' + cik;
     }
-    if (!skipList.includes(cik)) {
-      cluster.queue(cik);
-    } else {
-      console.log(`%c Skipped ${cik}`, 'color: grey');
-    }
+    ciks.push(cik);
   }
 
-
-  await cluster.idle();
+  for (let cik of ciks) {
+    const browser = await puppeteer.launch(
+      {
+        headless: false,
+        args: [`--window-size=${1920},${1080}`],
+      }
+    );
+    const page = await browser.newPage();
+    let linksMap = await findDocumentLinks(page, cik);
+    let links = linksMap.get('links');
+    let fileDates = linksMap.get('fileDates');
+    for (let i = 0; i < links.length; i++) {
+      // We want to pass a map to the cluster with a single value
+      let filingMap = new Map();
+      filingMap.set('link', links[i]);
+      filingMap.set('fileDate', fileDates[i]);
+      filingMap.set('cik', cik);
+      cluster.queue(filingMap);
+    }
+    await cluster.idle();
+  }
+  
   await cluster.close();
 
   console.log('%c Completed program!', 'color:green;');
@@ -79,32 +107,47 @@ async function test(url) {
  * @param {Cluster} cluster
  */
 async function initCluster(cluster) {
-  await cluster.task(async ({ page, data: cik }) => {
+  await cluster.task(async ({ page, data: filingMap }) => {
+    // A filing map needs to contain keys 'link', 'fileDate', and 'CIK',
+    // Both should be pointing to a SINGLE VALUE, not an array
+    const link = filingMap.get('link');
+    const fileDate = filingMap.get('fileDate');
+    const cik = filingMap.get('cik');
     try {
-      let documentCollection = await parseEdgarSearch(page, cik);
-      if (!documentCollection.hasData) {
-        throw new Error(`No data found in CIK ${cik}`);
+      // And now we have a full list of 10Q links!
+      console.log(`%cParsing filing page: ${link}`, 'color:orange');
+      const schedules = await tenQUtility.parse10Q(page, link);
+      const form = new TenQDoc(fileDate, schedules, link);
+      // Now we make a document for this filing.
+      let dir = `./production/${cik}/`;
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
       }
-      let outputString = documentCollection.toCsv();
-      fs.writeFile(`./output/${cik}.csv`, outputString, err => {
-        if (err) {
-          // console.error(err);
+      //For some reason, some filings share a date.
+      //So we check if it exists, and then give it a number if it shares a date.
+      let fileName = `${dir}${fileDate}`
+      while (fs.existsSync(fileName + '.csv')) {
+        let split = fileName.split('_');
+        if (split.length != 2) {
+          // In this case we haven't added any numbers yet.
+          fileName = fileName + '_1';
         } else {
-          // file written successfully
+          // We add one to the number. Just like Pro Tools does.
+          let num = Number(split[1]);
+          num++;
+          fileName = split[0] + `_${num}`;
         }
-      });
-      console.log(`%c Done parsing CIK ${cik}`, 'color: green');
+      }
+      fs.writeFileSync(
+        fileName + '.csv',
+        form.toCsv(),
+      );
+      addFilingToSkipList(link, cik);
+      console.log(`%cParsed!`, 'color:green');
+      console.log(`%cFINISHED CIK ${cik}`, 'color:green');
     } catch (e) {
-      let str = '';
-      str += e;
-      try {
-        fs.writeFileSync(
-          `./output/${cik}.txt`,
-          str
-        );
-      } catch (e) { }
-      console.error(`%c Failed to parse CIK ${cik}`);
-      return false;
+      console.error(e);
+      console.error('Failed to parse filing ', link)
     }
   });
 }
@@ -149,89 +192,6 @@ async function testPage(page, url) {
   } catch (e) {
     console.error(e);
   }
-}
-
-/**
- * 
- * @param {Page} page 
- * @param {string} search
- * @returns {Promise<TenQCollection>} the title of the forms found.
- */
-async function parseEdgarSearch(page, cik) {
-
-  // Navigate the page to a URL. Wait until the page is fully loaded.
-  await page.goto(`https://www.sec.gov/edgar/search/#/dateRange=custom&category=custom&entityName=${cik}&startdt=2004-01-01&enddt=2024-08-09&forms=10-Q`,
-    { waitUntil: 'networkidle0' }
-  );
-
-  //Here's the table containing all the forms. We'll get its element.
-  const hits = await page.$$('#hits > table > tbody > tr');
-
-  // console.log(`Hits length: ${hits.length}`);
-
-  let formList = [];
-  const tenQUtility = new TenQUtility();
-
-  // We can't be going back and forth from the search page
-  // So first we collect every link
-
-  let links = new Array();
-  let fileDates = new Array();
-
-  for (const hit of hits) {
-    //For the moment, we're going to try logging file dates
-    const fileDate = await page.evaluate(
-      (el) => el.querySelector('td.filed').textContent,
-      hit,
-    );
-    fileDates.push(fileDate);
-    //Cool, now we wanna click through each filetype box.
-    const hitHandle = await hit.$('td.filetype > a');
-    // console.log(hitHandle);
-    hitHandle.click();
-
-    await page.waitForSelector('#open-file', { timeout: 10000 });
-    let link;
-    while (!link) {
-      const buttonHandle = await page.$('#open-file');
-      // console.log('Button handle: ', buttonHandle);
-      const jsonLink = await buttonHandle.getProperty('href');
-      link = await jsonLink.jsonValue();
-    }
-    links.push(link);
-    // Now we have to close the preview page, so we find the button
-    // for that
-    await page.waitForSelector('#previewer > div > div > div.modal-header.border.border-0 > button', { timeout: 10000 });
-    const closeHandle = await page.$('#previewer > div > div > div.modal-header.border.border-0 > button');
-    try {
-      await closeHandle.click();
-    } catch (e) { }
-    // We add a delay because this seems to be the most intensive
-    // fetch, and where the SEC's most likely to block us.
-    await delay(1000);
-  }
-
-  // And now we have a full list of 10Q links!
-  for (let i = 0; i < links.length; i++) {
-
-    // console.log(`Parsing filing page: ${links[i]}`);
-    // console.log(`Link ${i}/${links.length}`);
-    const schedules = await tenQUtility.parse10Q(page, links[i]);
-    // console.log(`Schedules in: ${schedules}`);
-    const form = new TenQDoc(fileDates[i], schedules, links[i]);
-    formList.push(form);
-  }
-
-  // For testing
-  // const schedules = await tenQUtility.parse10Q(page, 'https://www.sec.gov/Archives/edgar/data/1099941/000110465918016749/a18-7739_110q.htm');
-  // console.log('successfully parsed schedules');
-  // const form = new TenQDoc(Date(Date.now()), schedules, 'https://www.sec.gov/Archives/edgar/data/1099941/000110465918016749/a18-7739_110q.htm');
-  // formList.push(form);
-
-  await page.close();
-  // console.log('Finished CIK ', cik);
-  return new TenQCollection(cik, formList);
-
 }
 
 /**
@@ -457,6 +417,6 @@ async function pushthroughEdgarSearch(page, cik) {
 
 }
 
-// main();
+main();
 // test('https://www.sec.gov/Archives/edgar/data/17313/000114036113041116/form10q.htm');
-pushthrough();
+// pushthrough();
